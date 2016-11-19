f8a2086f39ed8a206ac3b515fb998733b52dfc71
Surakiti Sopontanapat <asia_like_za@hotmail.com>
2016-11-19 22:36:49 +0700

    Part10New
    Project_JSP

--- Project_JSP/web/js/agefromdob.js
+++
@@ -1,7 +0,0 @@
-function calcAge() {
-    var Bdate = document.getElementById('dob').value;
-    var Bday = +new Date(Bdate);
-    Bdate += ((Date.now() - Bday) / (31557600000));
-    var theBday = document.getElementById('age');
-    theBday.innerHTML = Bdate;
-}
